04926413e43b82445090ea707e5e82408c9f96a7
zoet24 <zt12542@my.bristol.ac.uk>
2020-11-10 14:41:03 +0000

    Replace h1 on index.html with png of title to maintain spacing across different medias

--- assets/js/tutorial.js
+++ assets/js/tutorial.js
@@ -11,28 +11,28 @@ $(".carousel-control").click(function(){
 
 function slide(){
     setTimeout(function() {
-        $(".image-2").addClass("hide-button");
-        $(".image-3").removeClass("hide-button");
+        $(".image-2").addClass("hide-button", "style.css");
+        $(".image-3").removeClass("hide-button", "style.css");
     }, 50);
     setTimeout(function() {
-        $(".image-3").addClass("hide-button");
-        $(".image-4").removeClass("hide-button");
+        $(".image-3").addClass("hide-button", "style.css");
+        $(".image-4").removeClass("hide-button", "style.css");
     }, 300);
     setTimeout(function() {
-        $(".image-4").addClass("hide-button");
-        $(".image-5").removeClass("hide-button");
+        $(".image-4").addClass("hide-button", "style.css");
+        $(".image-5").removeClass("hide-button", "style.css");
     }, 1250);
     setTimeout(function() {
-        $(".image-5").addClass("hide-button");
-        $(".image-6").removeClass("hide-button");
+        $(".image-5").addClass("hide-button", "style.css");
+        $(".image-6").removeClass("hide-button", "style.css");
     }, 1500);
     setTimeout(function() {
-        $(".image-6").addClass("hide-button");
-        $(".image-7").removeClass("hide-button");
+        $(".image-6").addClass("hide-button", "style.css");
+        $(".image-7").removeClass("hide-button", "style.css");
     }, 1750);
 }
 
 function resetSlide(){
-    $(".image-7").addClass("hide-button");
-    $(".image-2").removeClass("hide-button");
+    $(".image-7").addClass("hide-button", "style.css");
+    $(".image-2").removeClass("hide-button", "style.css");
 }
