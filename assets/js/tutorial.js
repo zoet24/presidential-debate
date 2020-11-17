5481a859c2f230a646c2311eb8b980c10b6efcbb
zoet24 <zt12542@my.bristol.ac.uk>
2020-11-17 20:52:29 +0000

    Improve tutorial instructions

--- assets/js/tutorial.js
+++ assets/js/tutorial.js
@@ -1,7 +1,16 @@
 $(".carousel-control").click(function(){
     setTimeout(function() {
-        if($("#carousel-second-slide").hasClass('active')) {
-            slide();
+        if($("#carousel-third-slide").hasClass('active')) {
+            slideThird();
+        }
+        if($("#carousel-fourth-slide").hasClass('active')) {
+            slideFourth();
+        }
+        if($("#carousel-fifth-slide").hasClass('active')) {
+            slideFifth();
+        }
+        if($("#carousel-sixth-slide").hasClass('active')) {
+            slideSixth();
         }
         else {
             resetSlide();
@@ -9,30 +18,65 @@ $(".carousel-control").click(function(){
     }, 1000);
 });
 
-function slide(){
-    setTimeout(function() {
-        $(".image-2").addClass("hide-button", "style.css");
-        $(".image-3").removeClass("hide-button", "style.css");
-    }, 50);
+function slideThird(){
     setTimeout(function() {
         $(".image-3").addClass("hide-button", "style.css");
         $(".image-4").removeClass("hide-button", "style.css");
-    }, 300);
+    }, 50);
     setTimeout(function() {
         $(".image-4").addClass("hide-button", "style.css");
         $(".image-5").removeClass("hide-button", "style.css");
-    }, 1250);
-    setTimeout(function() {
-        $(".image-5").addClass("hide-button", "style.css");
-        $(".image-6").removeClass("hide-button", "style.css");
-    }, 1500);
+    }, 300);
+}
+
+function slideFourth(){
     setTimeout(function() {
         $(".image-6").addClass("hide-button", "style.css");
         $(".image-7").removeClass("hide-button", "style.css");
-    }, 1750);
+    }, 50);
+    setTimeout(function() {
+        $(".image-7").addClass("hide-button", "style.css");
+        $(".image-8").removeClass("hide-button", "style.css");
+    }, 300);
+}
+
+function slideFifth(){
+    setTimeout(function() {
+        $(".image-9").addClass("hide-button", "style.css");
+        $(".image-10").removeClass("hide-button", "style.css");
+    }, 50);
+    setTimeout(function() {
+        $(".image-10").addClass("hide-button", "style.css");
+        $(".image-11").removeClass("hide-button", "style.css");
+    }, 300);
+    setTimeout(function() {
+        $(".image-11").addClass("hide-button", "style.css");
+        $(".image-12").removeClass("hide-button", "style.css");
+    }, 550);
+}
+
+function slideSixth(){
+    setTimeout(function() {
+        $(".image-13").addClass("hide-button", "style.css");
+        $(".image-14").removeClass("hide-button", "style.css");
+    }, 50);
+    setTimeout(function() {
+        $(".image-14").addClass("hide-button", "style.css");
+        $(".image-15").removeClass("hide-button", "style.css");
+    }, 300);
+    setTimeout(function() {
+        $(".image-15").addClass("hide-button", "style.css");
+        $(".image-16").removeClass("hide-button", "style.css");
+    }, 550);
 }
 
 function resetSlide(){
-    $(".image-7").addClass("hide-button", "style.css");
-    $(".image-2").removeClass("hide-button", "style.css");
+    $(".image-5").addClass("hide-button", "style.css");
+    $(".image-3").removeClass("hide-button", "style.css");
+    $(".image-8").addClass("hide-button", "style.css");
+    $(".image-6").removeClass("hide-button", "style.css");
+    $(".image-12").addClass("hide-button", "style.css");
+    $(".image-9").removeClass("hide-button", "style.css");
+    $(".image-16").addClass("hide-button", "style.css");
+    $(".image-13").removeClass("hide-button", "style.css");
 }
