b02db63a5f5da3452af07bf830dc30b4d4d52e0e
zoet24 <zt12542@my.bristol.ac.uk>
2020-11-21 17:48:49 +0000

    Add landscape prompt to index.html

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,28 @@
+// Check if device is in portrait mode
+onload = function () {
+    if (window.innerWidth < window.innerHeight == true) { // Portrait
+        $(".container-portrait-overlay").removeClass("hide-button", "style.css");
+        $("#home-title").addClass("hide-button", "style.css");
+		$("#home-cta-start").addClass("hide-button", "style.css");
+		$("#home-cta-tutorial").addClass("hide-button", "style.css");
+    }
+};
+
+// Listen for orientation changes - source: https://stackoverflow.com/questions/40257329/on-mobile-how-can-i-display-an-overlay-when-the-screen-orientation-is-in-landsc
+window.addEventListener("orientationchange", function() {
+    if (window.innerWidth > window.innerHeight == true) {
+        $(".container-portrait-overlay").removeClass("hide-button", "style.css");
+        $("#home-title").addClass("hide-button", "style.css");
+        $("#home-cta-start").addClass("hide-button", "style.css");
+        $("#home-cta-tutorial").addClass("hide-button", "style.css");
+    } else {
+        $(".container-portrait-overlay").addClass("hide-button", "style.css");
+        $("#home-title").removeClass("hide-button", "style.css");
+        $("#home-cta-start").removeClass("hide-button", "style.css");
+        $("#home-cta-tutorial").removeClass("hide-button", "style.css");
+    }
+});
+
 $(".start-button").click(function () { // Animation for index.html --> game.html transition
 	var audio = document.getElementById("audio-wwe-bell");
 	audio.currentTime = 0;
